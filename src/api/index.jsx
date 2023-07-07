cac6cdfb9b1f4f2b1c4a2e472a97672c7f29ca8c
unknown <hamidazad020@gmail.com>
2023-07-07 20:59:27 +0600

    update code

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const fetcher = axios.create({
-  baseURL: "https://ifamilymart-client.onrender.com",
+  // baseURL: "https://ifamilymart-client.onrender.com",
+  baseURL: "http://localhost:8000",
 });
 
 export default fetcher;
